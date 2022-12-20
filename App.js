9177cf2560627178b22fb1542a73d6ad966f3392
Tsai-Ching <h93fu4@gmail.com>
2022-12-20 13:18:55 +0800

    updating

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
+
 const { Configuration, OpenAIApi } = require("openai");
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-// create LINE SDK config from env variables
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
+// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
@@ -34,7 +34,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-async function handleEvent(event) {
+sync function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
@@ -43,11 +43,11 @@ async function handleEvent(event) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: event.message.text ,
-    max_tokens: 200,
+    max_tokens: 500,
   });
 
   // create a echoing text message
-  const echo = { type: 'text', text: completion.data.choices[0].text };
+  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
@@ -58,4 +58,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
-
